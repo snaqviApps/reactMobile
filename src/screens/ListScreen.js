61b5fab0148d465657e7d23e230c51d7f9816bda
abbas <naqvi.sdsu@gmail.com>
2020-06-17 21:45:43 -0500

    added ListScreen using FlatList Element

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Text, StyleSheet, View, Button, Alert, FlatList } from 'react-native';
+
+const ListScreen = ()=> {                           // Component 'ListScreen' starts here, uses 'FlatList Element'
+  const friends = [                                 // Needs two 'Props': 'data' and 'renderItem'
+    {name: 'Friend-A'},
+    {name: 'Friend-B'},
+    {name: 'Friend-C'},
+    {name: 'Friend-D'},
+    {name: 'Friend-E'}
+];
+  return (
+    <FlatList
+    data={friends}
+    // renderItem={(element) => {}} />              // Not using 'element' but it's 'prop' called 'item'
+    renderItem={({ item }) => {                     // element === { item: {name: 'Friend-A'}, index: 0 }
+      return <Text>{ item.name }</Text>;
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
